c0a7ee3e33b91f1a1db4675fae8eed1ad1b448c5
Allison Logan <allisonlogan@Logan-Family-iMac.local>
2021-02-22 11:56:19 -0500

    Fixes scrolling bug when using pagination.

--- js/hwcoesyllabi.js
+++ js/hwcoesyllabi.js
@@ -5,10 +5,17 @@
 jQuery(function($) {
 	
 	//Data Tables
-	$('#syllabi-table').DataTable( {
+	var table = $('#syllabi-table').DataTable( {
         "dom": 'lf<"table-wrapper"t>ip',
 		//responsive: true,
 		"pageLength": 50
 	});
 	
+	table.on('page.dt', function() {
+		$('html, body').animate({
+			scrollTop: $(".dataTables_wrapper").offset().top -200
+		}, 'slow');
+	});
+	
 });
+
